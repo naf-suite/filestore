d29c1707f59196a41353b151ce9dcc86f82ac20f
dygapp <dygapp@qq.com>
2019-11-02 11:17:43 +0800

    增加dev环境配置文件

---
+++ config/config.dev.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const { sep } = require('path');
+
+module.exports = appInfo => {
+  const config = (exports = {});
+
+  config.cdn = {
+    repos_root_path: `${appInfo.baseDir}${sep}upload`,
+    repos_root_url: '/files',
+  };
+
+  return config;
+};
